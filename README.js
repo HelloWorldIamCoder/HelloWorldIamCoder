4c337d082feca5916aa9e4df307dcb2ba06318e1
HelloWorldIamCoder <155084662+HelloWorldIamCoder@users.noreply.github.com>
2023-12-28 21:17:06 +0530

    Create README.js
    ME

---
+++ README.js
@@ -0,0 +1,29 @@
+- 👋 Hi, I’m @HelloWorldIamCoder
+- 👀 I’m interested in ... coding
+- 🌱 I’m currently learning ... HTML/CSS/JAVASCRIPT
+- 💞️ I’m looking to collaborate on ... IDK
+- 📫 How to reach me ... my gmail is gamerhalo2124@gmail.com
+- N also, im a 9 year old so i learned something about data handling in math so i wrote some code about it. See the following.
+
+console.log('Every fruit represents 5 in its fruit or type');
+let apple = 3;
+console.log('The apples are 3');
+apple = 5 * 3;
+console.log('Total apples are equal to ' + apple);
+let oranges = 3;
+console.log('The oranges are 3');
+oranges = 5 * 3;
+console.log('Total oranges are equal to ' + oranges);
+let mango = 5;
+console.log('The mangoes are 5');
+mango = 5 * 5;
+console.log('Total mangoes are equal to ' + mango);
+let pear = 2;
+console.log('The pears are 2');
+pear = 2 * 5;
+console.log('Total pears are equal to ' + pear);
+let watermelon = 1;
+console.log('The watermelons are 1');
+watermelon = 5 * 1;
+console.log('Total watermelons are equal to ' + watermelon);
+
